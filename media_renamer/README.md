96549ce99a21b18fd614323a877fe22726d1d088
eiriksnedland <eiriksnedland@gmail.com>
2026-05-23 14:00:28 +0200

    Upload

---
+++ media_renamer/README.md
@@ -0,0 +1,35 @@
+# Bulk Name Changer
+Change filenames
+
+## How to use manual
+Enter the configuration in main file. 
+* NAME_PREFIX new file name
+* DEFAULT_SEASON needs to be set if the current filenames does not include season number
+* LANGUAGE should be set if it is a .srt file 
+
+The files to be changed needs to be added to the Items folder. Another option is to change the TARGET_DIRECTORY to the path of desired directory.
+
+Save the file and run it from terminal.
+Run in terminal:
+```python .\renamer_script.py ```
+ or
+```py .\renamer_script.py ```
+
+
+## How to use with docker compose
+Enter config in compose file
+* NAME_PREFIX 
+* DEFAULT_SEASON 
+* LANGUAGE 
+
+Add files to items folder
+
+Run in terminal:
+```docker compose up --build```
+
+
+
+## Testing
+TestCases folder includes the test scenarioes the script was testet on. Add more if nessecary and copy over to items folder. Run script and see if it works as intended.
+
+Make sure all files have prefix. The file in testdata that ends with .5 without .txt after will end up as "-part2.5" isntead of "-part2".
---
+++ media_renamer/renamer_script.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from helpers.nameChanger import get_half_episodes, format_new_name, fix_subtitle_language
+
+# --- KONFIGURASJON ---
+TARGET_DIRECTORY = os.environ.get("TARGET_DIR", "./items")
+
+# Reads from environment variables, falls back to your defaults if missing
+NAME_PREFIX = os.environ.get("NAME_PREFIX", "filename")
+DEFAULT_SEASON = int(os.environ.get("DEFAULT_SEASON", "1"))
+LANGUAGE = os.environ.get("LANGUAGE", "en")
+
+
+def main():
+    if not os.path.isdir(TARGET_DIRECTORY):
+        print(f"Error: Directory {TARGET_DIRECTORY} not found.")
+        return
+
+    print(f"Starting processing with Prefix: '{NAME_PREFIX}', Season: {DEFAULT_SEASON}, Lang: '{LANGUAGE}'")
+    half_episodes = get_half_episodes(TARGET_DIRECTORY)
+
+    for filename in os.listdir(TARGET_DIRECTORY):
+        old_path = os.path.join(TARGET_DIRECTORY, filename)
+        
+        if os.path.isfile(old_path):
+            # 1. Finn nytt navn (Sesong/Episode logikk)
+            new_name = format_new_name(filename, half_episodes, DEFAULT_SEASON, NAME_PREFIX)
+            
+            if new_name:
+                new_path = os.path.join(TARGET_DIRECTORY, new_name)
+                
+                # Utfør omdøping hvis navnet er annerledes
+                if filename != new_name:
+                    print(f"Renaming: {filename} -> {new_name}")
+                    os.rename(old_path, new_path)
+                    current_file_path = new_path
+                else:
+                    current_file_path = old_path
+
+                # 2. Sjekk for språktag hvis det er en .srt fil
+                if current_file_path.endswith(".srt"):
+                    fix_subtitle_language(current_file_path, LANGUAGE)
+
+if __name__ == "__main__":
+    main()
